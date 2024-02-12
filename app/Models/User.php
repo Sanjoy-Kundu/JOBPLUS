aa3ac7464599a1d02e5678370c501dfceef0fcef
unknown <sanjoykundu187@gmail.com>
2024-02-12 23:40:26 +0600

    user multiple rore migration table and database create done

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,14 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
     ];
+
+
+ protected $attributes = ['role' => 'cadidates'];
+
+
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ resources/views/notes.js
@@ -0,0 +1,6 @@
+/* 
+1. role management er jonno ami create_users_table manage korlam 
+2.amra web.php diye login route toyri kori 
+ 
+
+*/
