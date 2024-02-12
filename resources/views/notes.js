db0b5d4348165db0c20bc7bffd81f1d363860a47
unknown <sanjoykundu187@gmail.com>
2024-02-13 01:21:40 +0600

    noteds added

--- resources/views/notes.js
+++ resources/views/notes.js
@@ -1,6 +1,7 @@
 /* 
 1. role management er jonno ami create_users_table manage korlam 
 2.amra web.php diye login route toyri kori 
+3. candidate + admin + companies added with profile deletes by postman
  
 
 */
